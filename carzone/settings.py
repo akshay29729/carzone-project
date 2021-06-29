aa813c32b057a2eec0c7eefa5e40b09c40d485ae
akshay29729 <akshayshinde29729@gmail.com>
2021-06-29 10:58:00 +0530

    configered database and database_url

--- carzone/settings.py
+++ carzone/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 from django.contrib.messages import constants as messages
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p@5l2k8c3fwc#+%n0p$#)w#bttq&ac$g$#&naapf2x)&dlk6o5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoise.Middleware',
 ]
 
 ROOT_URLCONF = 'carzone.urls'
@@ -87,15 +89,17 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carzone_db',
-        'USER': 'postgres',
-        'PASSWORD': 'akshay29729',
-        'HOST': 'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'carzone_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'akshay29729',
+#         'HOST': 'localhost',
+#     }
+# }
+DATABASES = {'default': dj_database_url.config(
+    default='postgres://postgres:akshay29729@localhost/carzone_db')}
 
 
 # Password validation
@@ -156,3 +160,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'akshayshinde29729@gmail.com'
 EMAIL_HOST_PASSWORD = '1234abcd'
 EMAIL_USE_TLS = True
+
+# WHitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressesManifestStaticFilesStorage'
